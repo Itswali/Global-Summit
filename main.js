118a8682b5742c9143d720de17bbdb17e8ef1d68
Wali-Dexter <luckywali0986@gmail.com>
2022-10-25 11:11:10 +0500

    add speakers content with projects content

---
+++ main.js
@@ -0,0 +1,48 @@
+const speakers = [
+
+    {
+        speakerAvartar: 'Images/sp1.png',
+        speakerName: 'Yonchai Benkler',
+        speakerProfession:
+          'Berkman Professor of Enterpreneurial Legal Studoes at Havard Law School',
+        speakerDescription:
+          'Benkler studied common-based peer production and published his seminal book. The Wealth of Networks in 2006',
+    },
+    {
+        speakerAvartar: 'Images/sp2.png',
+        speakerName: 'Joen Gil-nam',
+        speakerProfession: 'Doctor in Psychology and Mental Health',
+        speakerDescription:
+          'An academia from  Oxford University graduated with a doctorate in Mental health and phychology',
+    },
+    {
+        speakerAvartar: 'Images/sp3.png',
+        speakerName: 'Julia Leda',
+        speakerProfession:
+          'Minister of Human Rights Representing Asia in the UN board Committee',
+        speakerDescription:
+          'Appointed by the UN President presiding the previous change of UN cabinet on Human security matters',
+    },
+    {
+        speakerAvartar: 'Images/sp4.png',
+        speakerName: 'Layla Tretikov',
+        speakerProfession: 'Professor in Climatology and Climate Change',
+        speakerDescription:
+          'Studied for Climate and Climatology from The University of London and part of the Global warming research team.',
+    },
+    {
+        speakerAvartar: 'Images/sp5.png',
+        speakerName: 'Ryan Merjkey',
+        speakerProfession:
+          'Merjkey Professor of Enterpreneurial Legal Studoes at Havard Law School',
+        speakerDescription:
+          'Ryan studied common-based peer production and published his seminal book. The Wealth of Networks in 2006',
+    },
+    {
+        speakerAvartar: 'Images/sp6.png',
+        speakerName: 'Noh So-Young',
+        speakerProfession: 'Immigration and Migration Minister in the UN cabinaet',
+        speakerDescription:
+          'Young studied common-based peer production and published his seminal book. The Wealth of Networks in 2006',
+    },
+];
