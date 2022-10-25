040aaa43e10d7a59911ee55fb5cf028347d26c4d
Wali-Dexter <luckywali0986@gmail.com>
2022-10-26 03:21:14 +0500

    add linters error and fix it

--- nav.js
+++ nav.js
@@ -2,9 +2,7 @@ const hamburger = document.querySelector('.bars');
 const overlay = document.querySelector('.overlay');
 const mainMenu = document.querySelector('.main-menu');
 const cancel = document.querySelector('.cancel');
-const links = document.querySelector('.menu-item');
-const cardsSection = document.querySelector('.middle');
-const modalsSection = document.querySelector('.modal-container');
+// const links = document.querySelector('.menu-item');
 
 hamburger.addEventListener('click', () => {
   mainMenu.style.display = 'flex';
