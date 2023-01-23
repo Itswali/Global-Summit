e1d60bb98afe8832c082983a962febe9f30d91fc
Wali Muhammad <luckywali0986@gmail.com>
2023-01-23 21:09:14 +0500

    Apply best practices on code

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const speakers = [
     speakerDescription:
         'Benkler studied common-based peer production and published his seminal book.The Wealth of Networks in 2006',
   },
+
   {
     speakerAvartar: 'Images/sp2.png',
     speakerName: 'Joen Gil-nam',
@@ -15,6 +16,7 @@ const speakers = [
     speakerDescription:
             'An academia from  Oxford University graduated with a doctorate in Mental health and phychology',
   },
+
   {
     speakerAvartar: 'Images/sp3.png',
     speakerName: 'Julia Leda',
@@ -23,6 +25,7 @@ const speakers = [
     speakerDescription:
         'Appointed by the UN President presiding the previous change of UN cabinet on Human security matters',
   },
+
   {
     speakerAvartar: 'Images/sp4.png',
     speakerName: 'Layla Tretikov',
@@ -30,6 +33,7 @@ const speakers = [
     speakerDescription:
             'Studied for Climate and Climatology from The University of London and part of the Global warming research team.',
   },
+
   {
     speakerAvartar: 'Images/sp5.png',
     speakerName: 'Ryan Merjkey',
@@ -38,12 +42,14 @@ const speakers = [
     speakerDescription:
         'Ryan studied common-based peer production and published his seminal book.The Wealth of Networks in 2006',
   },
+
   {
     speakerAvartar: 'Images/sp6.png',
     speakerName: 'Noh So-Young',
     speakerProfession: 'Immigration and Migration Minister in the UN cabinaet',
     speakerDescription: 'Young studied common-based peer production and published his seminal book.The Wealth of Networks in 2006',
   },
+
 ];
 
 const speakerDiv = document.getElementById('speakingInt');
